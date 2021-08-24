f5bba46c0c1c5aaab13a974d176e17f35d4330b4
primaryCoode <51342199+primaryCoode@users.noreply.github.com>
2021-08-24 11:42:01 -0300

    Add files via upload

---
+++ SpeakHello.js
@@ -0,0 +1,10 @@
+
+(function (window) {
+  var helloSpeaker = {}
+  helloSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+  }
+  var speakWord = "Hello";
+  window.helloSpeaker = helloSpeaker;
+
+}) (window);
